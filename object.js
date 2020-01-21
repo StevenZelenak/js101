0382d14c8beddf9fe5c445fe5971a4bcea6f1341
StevenZelenak <stevenmzelenak@gmail.com>
2020-01-20 19:39:41 -0800

    updating object file

---
+++ object.js
@@ -0,0 +1,21 @@
+console.log("Hi my friends");
+
+
+const wordLenght = (word) =>{
+    return word.length;
+}
+
+console.log(wordLenght('cat'))
+
+const ageCheck = (age) =>{
+
+        if(age >= 21){
+            return 'PARTY!!!!!!!!!';
+        }
+
+        return 'no drinks for you!';
+
+}
+
+console.log(ageCheck(12));
+console.log(ageCheck(22));
