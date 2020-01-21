89937e5000c0e6e8927caf5a607c30ec69ca57d6
StevenZelenak <stevenmzelenak@gmail.com>
2020-01-20 21:05:36 -0800

    adding array file in

---
+++ arrays.js
@@ -0,0 +1,16 @@
+// console.log("I am here");
+
+// const junkDrawer = ['hi',2,3,4,true,{name: 'zoe'},[1,2,3,4]];
+
+// console.log(junkDrawer[2]);
+// junkDrawer.push('fluffy');
+
+
+const valuePrinter = (arr, ind) => {
+
+    return arr[ind];
+
+}
+
+console.log(valuePrinter([1,2,3,4,5], 2));
+console.log(valuePrinter(['cat','dog','bear','turtle','fish'], 3));
