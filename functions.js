a06bda925f4b0c1ce1bb54430751d57362ce1417
StevenZelenak <stevenmzelenak@gmail.com>
2020-01-20 19:10:24 -0800

    finished function challenge

--- functions.js
+++ functions.js
@@ -17,3 +17,22 @@ dogBreed = (dogBreed) =>{
 
 console.log(dogBreed('lab'));
 console.log(dogBreed('monkey'));
+
+
+fortyTwo = (number) => {
+ const output = number + 42;
+ return output;
+}
+
+console.log(fortyTwo(2));
+console.log(fortyTwo(42));
+
+
+oldAge = (yearBorn) => {
+const age = 2099 - yearBorn;
+const output = `You will be ${age} years old in 2099!`;
+return output;
+}
+
+console.log(oldAge(1954));
+console.log(oldAge(1991));
