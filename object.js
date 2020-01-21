4008faea17f6a6f5fd6092ddc31dc5b1dbbc9ee3
StevenZelenak <stevenmzelenak@gmail.com>
2020-01-20 20:44:16 -0800

    adding objects file

--- object.js
+++ object.js
@@ -19,3 +19,50 @@ const ageCheck = (age) =>{
 
 console.log(ageCheck(12));
 console.log(ageCheck(22));
+
+
+const expense = {
+
+    dateCreated: '01/17/2020',
+    location: 'Baja Burrito',
+    cost: 12.57,
+    isGood: true,
+}
+
+console.log(expense.cost);
+console.log(expense['cost']);
+
+
+const employee = {
+
+    company: 'Walmart',
+    position: 'Manager',
+    wage: 14.00,
+    wearsUniform: true,
+    yearsWorked: 2,
+    firstName: 'Steve',
+    lastName: 'Wild',
+
+
+}
+
+
+const statusMaker = (object) => {
+
+    if(object.firstName[0] === 'M' || object.firstName[0] === 'm')
+    {
+        object.status = 'vip';
+        
+    }
+    else{
+        
+        object.status = 'peasant';
+        
+    }
+
+    return object;
+}
+
+statusMaker(employee);
+
+console.log(employee);
